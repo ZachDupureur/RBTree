af3311031d061ff5eaed1c3756bc1561066d94ff
Zach Dupureur <dupurezt4463@student.laccd.edu>
2022-03-11 16:17:33 -0800

    03/11/2022

--- header.h
+++ header.h
@@ -1,8 +1,21 @@
-//
-// Created by zach on 3/11/22.
-//
+/// Created by Zach Dupureur on 3/11/22.
+/// Decided to restructure my binary tree this time.
+
 
 #ifndef RBTREE_HEADER_H
 #define RBTREE_HEADER_H
+
+#include <iostream>
+
+class Node {
+public:
+    double data = 0;
+    Node * left = nullptr;
+    Node * right = nullptr;
+    explicit Node(double i);
+};
+
+
+void insert(Node* root, double key);
 
 #endif //RBTREE_HEADER_H
--- implementation.cpp
+++ implementation.cpp
@@ -1,4 +1,47 @@
-//
-// Created by zach on 3/11/22.
-//
+/// Created by Zach Dupureur on 3/11/22.
 
+#include "header.h"
+
+ void insert(Node* root, double key) {
+     Node *temp = root;
+     Node *newNode = new Node(key);
+     if (temp == nullptr) { // Tree is empty, make new node root
+         root = newNode;
+     }
+     else { // Tree not empty
+         while (temp != nullptr) { // Search for leaf
+             if (temp->data == newNode->data) { // Duplicate data
+                 std::cout << "Cannot insert. Duplicate data.\n";
+                 break;
+             }
+             else {
+                 if (newNode->data < temp->data) { // If smaller go to left
+                     if (temp->left == nullptr) { // Add node if nullptr
+                         temp->left = newNode;
+                         temp = nullptr;
+                     }
+                     else {
+                         temp = temp->left;
+                     }
+                 }
+                 else {
+                     if (newNode->data > temp->data) { // If larger go to right
+                         if (temp->right == nullptr) { // Add node if nullptr
+                             temp->right = newNode;
+                             temp = nullptr;
+                         }
+                         else {
+                             temp = temp->right;
+                         }
+                     }
+                 }
+             }
+         }
+     }
+ }
+
+Node::Node(double i) {
+    right = nullptr;
+    left = nullptr;
+    data = i;
+}
